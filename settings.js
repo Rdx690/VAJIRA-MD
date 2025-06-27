2c393586451c13b3fa31770263991f714a5bf566
Rdx690 <sourajitsarkar59@gmail.com>
2025-06-27 09:03:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94719199757' : process.env.OWNER_NUMBER,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=CohAiKrS#RD_0yw3s0TWTmpXKEJ2UBQplOPdcGWp2_SXTaknZzt8' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '916909950583' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
-PREFIX: process.env.PREFIX || '.' ,
-LOGO: process.env.LOGO || '' ,    
+PREFIX: process.env.PREFIX || ',' ,
+LOGO: process.env.LOGO || 'https://files.catbox.moe/j0ve33.jpg' ,    
 FOOTER: process.env.FOOTER || '',
 LIMIT_PLUGIN: process.env.LIMIT_PLUGIN || '50' ,    
-PREMIUM_USERS: process.env.PREMIUM_USERS || '94719199757',    
+PREMIUM_USERS: process.env.PREMIUM_USERS || '919089659697',    
 LANG: process.env.LANG || 'en',    
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
